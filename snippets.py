76319f21866d8c2c7203151cd57f4e8978f00e95
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-08-13 08:29:04 -0400

    setting up snippets.py per assignment

---
+++ snippets.py
@@ -0,0 +1,9 @@
+"""
+App for storing and retrieving snippets of text
+"""
+
+import logging
+import csv
+
+#Set the log output file and the log level
+logging.basicConfig(filename="output.log", level=logging.DEBUG)
